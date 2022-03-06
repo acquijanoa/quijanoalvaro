dafe2f2fe86b611c8d4a8b040354a0645c8e95c9
acquijanoa <acquijanoa@unal.edu.co>
2022-03-05 23:40:12 -0500

    prettierc included

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-const toggleButton = document.querySelector('.toggle-button')
-const bar = document.querySelector('.bar')
-const navbarLinks = document.querySelector('.navbar-links')
+'use strict';
+
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
 
 toggleButton.addEventListener('click', () => {
     if ('active' in navbarLinks.classList){
         navbarLinks.classList.delete('active');
         console.log(navbarLinks.classList);
     }else{
-        navbarLinks.classList.add('active');
+        navbarLinks.classList.add("active");
     }
 });
 
