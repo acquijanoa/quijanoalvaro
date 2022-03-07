8b25132836023bf097af7571683feb05090b551e
acquijanoa <acquijanoa@unal.edu.co>
2022-03-07 08:48:48 -0500

    toggle function in JS

--- js/index.js
+++ js/index.js
@@ -4,12 +4,7 @@ const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 
 toggleButton.addEventListener('click', () => {
-    if ('active' in navbarLinks.classList){
-        navbarLinks.classList.delete('active');
-        console.log(navbarLinks.classList);
-    }else{
-        navbarLinks.classList.add("active");
-    }
+    navbarLinks.classList.toggle('active');
 });
 
 
