687e099014bb132acb51262f26703f192fd5abcc
acquijanoa <acquijanoa@unal.edu.co>
2022-03-20 11:46:28 -0400

    fixed navbar

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@
 
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
+const navbar = document.querySelector('.navbar');
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
 
